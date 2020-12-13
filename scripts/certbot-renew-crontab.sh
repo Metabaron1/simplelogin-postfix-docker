15df80afc503bed1444b1cd91bc987fc64b93705
Metabaron1 <70103029+Metabaron1@users.noreply.github.com>
2020-12-13 19:02:44 +0100

    Update certbot-renew-crontab.sh

--- scripts/certbot-renew-crontab.sh
+++ scripts/certbot-renew-crontab.sh
@@ -4,8 +4,10 @@ set -e
 CERTIFICATE="/etc/letsencrypt/live/$POSTFIX_FQDN/fullchain.pem"
 PRIVATE_KEY="/etc/letsencrypt/live/$POSTFIX_FQDN/privkey.pem"
 
-if [ -f $CERTIFICATE -a -f $PRIVATE_KEY ]; then
-    /usr/bin/certbot -n renew 2> /dev/null
-else
-    /usr/bin/certbot -n certonly
-fi
+#if [ -f $CERTIFICATE -a -f $PRIVATE_KEY ]; then
+#    /usr/bin/certbot -n renew
+#else
+#    /usr/bin/certbot -n certonly
+#fi
+
+/usr/bin/certbot -n certonly > /dev/null 2>&1
